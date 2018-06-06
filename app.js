fdcd813befa312c617ca1317de048f480d7610e7
Justin Brown <justin.v.brown@gmail.com>
2018-06-06 18:58:30 -0500

    Edited app.js

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+mongoose.connect('mongodb://localhost/quandary')
+  .then(() =>  console.log('Connected to database!'))
+  .catch((err) => console.error(err));
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+var quandaries = require('./routes/quandaries');
 
 var app = express();
 
@@ -13,18 +20,22 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', index);
+app.use('/users', users);
+app.use('/quandaries', quandaries);
 
 /// Catch 404 and forward to error handler ///
 app.use(function(req, res, next) {
-  next(createError(404));
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 /// Error handler ///
